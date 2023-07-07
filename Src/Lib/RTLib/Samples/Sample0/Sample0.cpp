4e555bf905fc6df66b90364352567767cbcb13c5
sugita-shumpei <sugita.shumpei@fj.ics.keio.ac.jp>
2023-07-08 01:18:28 +0900

    Fix

--- Src/Lib/RTLib/Samples/Sample0/Sample0.cpp
+++ Src/Lib/RTLib/Samples/Sample0/Sample0.cpp
@@ -19,6 +19,7 @@
 //glm
 #include <glm/gtx/string_cast.hpp>
 #include <glm/gtc/matrix_integer.hpp>
+#include <glm/gtx/euler_angles.hpp>
 //glfw
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
@@ -38,34 +39,34 @@
 int main(int argc, const char** argv)
 {
 	auto importer = Assimp::Importer();
-	importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, false);
-
-
-	auto data_path = SAMPLE_SAMPLE0_DATA_PATH"\\Models\\Bistro_v5_2\\BistroExterior.fbx";
+	//importer.SetPropertyBool(AI_CONFIG_IMPORT_FBX_PRESERVE_PIVOTS, false);
+	importer.SetPropertyFloat(AI_CONFIG_GLOBAL_SCALE_FACTOR_KEY, 0.01f);
+	auto data_path = SAMPLE_SAMPLE0_DATA_PATH"\\Models\\ZeroDay\\MEASURE_ONE\\MEASURE_ONE.fbx";
+	//auto data_path = SAMPLE_SAMPLE0_DATA_PATH"\\Models\\Bistro_v5_2\\BistroExterior.fbx";
 	auto data_root = SAMPLE_SAMPLE0_DATA_PATH"\\Models\\ZeroDay";
 
 	unsigned int flag = 0;
-	//flag |= aiProcess_PreTransformVertices;
+	flag |= aiProcess_PreTransformVertices;
 	flag |= aiProcess_Triangulate;
 	//flag |= aiProcess_GenSmoothNormals;
 	flag |= aiProcess_GenUVCoords;
 	flag |= aiProcess_GenBoundingBoxes;
 	//flag |= aiProcess_OptimizeMeshes;
 
-	auto scene = importer.ReadFile(data_path,flag);
-	
-	RTLib::Int32 width  = 1024;
+	auto scene = importer.ReadFile(data_path, flag);
+
+	RTLib::Int32 width = 1024;
 	RTLib::Int32 height = 0;
 
-	std::unordered_map<aiNode*, aiMatrix4x4> modelMatrixMap = {};
+	std::unordered_map<aiNode*, RTLib::Matrix4x4> modelMatrixMap = {};
 	std::vector<GLuint> meshVaos = {};
 	std::vector<GLuint> meshVbos = {};
-	std::vector<std::pair<GLuint,GLsizei>> meshIbos = {};
-	
+	std::vector<std::pair<GLuint, GLsizei>> meshIbos = {};
+
 	{
 		std::stack<aiNode*> nodes = {};
 		nodes.push(scene->mRootNode);
-		modelMatrixMap[scene->mRootNode] = scene->mRootNode->mTransformation;
+		modelMatrixMap[scene->mRootNode] = glm::transpose(std::_Bit_cast<RTLib::Matrix4x4>(scene->mRootNode->mTransformation));
 
 		while (!nodes.empty()) {
 			auto node = nodes.top();
@@ -73,12 +74,14 @@ int main(int argc, const char** argv)
 			auto numChildren = node->mNumChildren;
 			for (auto i = 0; i < numChildren; ++i) {
 				auto child = node->mChildren[i];
-				auto parentTransform  = modelMatrixMap.at(node);
-				auto childTransform   = child->mTransformation;
+				auto parentTransform = modelMatrixMap.at(node);
+				auto childTransform  = glm::transpose(std::_Bit_cast<RTLib::Matrix4x4>(child->mTransformation));
 				modelMatrixMap.insert({ child, parentTransform * childTransform });
 				nodes.push(child);
-			}	
+			}
 		}
+
+
 	}
 	glfwInit();
 	{
@@ -118,10 +121,21 @@ int main(int argc, const char** argv)
 						vertices[8 * j + 4] = mesh->mNormals[j][1];
 						vertices[8 * j + 5] = mesh->mNormals[j][2];
 					}
+					else {
+
+						vertices[8 * j + 3] = 0.0f;
+						vertices[8 * j + 4] = 0.0f;
+						vertices[8 * j + 5] = 0.0f;
+					}
 					if (mesh->HasTextureCoords(0))
 					{
 						vertices[8 * j + 6] = mesh->mTextureCoords[0][j][0];
 						vertices[8 * j + 7] = mesh->mTextureCoords[0][j][1];
+					}
+					else {
+
+						vertices[8 * j + 6] = 0.5f;
+						vertices[8 * j + 7] = 0.5f;
 					}
 					//std::cout << "V: " << vertices[8 * j + 0] << "," << vertices[8 * j + 1] << "," << vertices[8 * j + 2] << std::endl;
 				}
@@ -161,7 +175,6 @@ int main(int argc, const char** argv)
 				gl->BindVertexArray(0);
 				gl->BindBuffer(GL_ARRAY_BUFFER, 0);
 				gl->BindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 			}
 		}
 
@@ -237,92 +250,137 @@ int main(int argc, const char** argv)
 			gl->DetachShader(prg, fs);
 		}
 
-		auto modelPos = gl->GetUniformLocation(prg, "model");
+		auto modelPos    = gl->GetUniformLocation(prg, "model");
 		auto viewProjPos = gl->GetUniformLocation(prg, "viewProj");
-		auto colorPos = gl->GetUniformLocation(prg, "color");
+		auto colorPos    = gl->GetUniformLocation(prg, "color");
 
 		auto cameraNode = scene->mRootNode->FindNode(scene->mCameras[0]->mName.C_Str());
-
-		auto viewMatrix = RTLib::Matrix4x4();
-		auto tmp = aiMatrix4x4();
-		camera->GetCameraMatrix(tmp);
-		auto tmp2 = modelMatrixMap[cameraNode] ;
-		std::memcpy(&viewMatrix, &tmp2, sizeof(tmp2));
-
-		viewMatrix = glm::inverse(glm::transpose(viewMatrix));
-		auto projMatrix = glm::perspective(
-			camera->mHorizontalFOV, camera->mAspect, camera->mClipPlaneNear, camera->mClipPlaneFar
+		auto lookAtLen  = camera->mLookAt.Length();
+		auto upLen      = camera->mUp.Length();
+		auto viewMatrix = glm::lookAt(
+			glm::vec3(camera->mPosition[0], camera->mPosition[1], camera->mPosition[2]),
+			glm::vec3(camera->mPosition[0], camera->mPosition[1], camera->mPosition[2]) +
+			glm::vec3(camera->mLookAt[0]  , camera->mLookAt[1]  , camera->mLookAt[2]  ),
+			glm::vec3(camera->mUp[0]      , camera->mUp[1]      , camera->mUp[2]      )
 		);
-		auto viewProjMatrix = RTLib::Core::Matrix4x4();
-		{
-			viewMatrix = glm::lookAt(
-				glm::vec3(camera->mPosition[0], camera->mPosition[1], camera->mPosition[2]),
-				glm::normalize(glm::vec3(camera->mLookAt[0], camera->mLookAt[1], camera->mLookAt[2])),
-				glm::normalize(glm::vec3(camera->mUp[0], camera->mUp[1], camera->mUp[2]))
-			);
+		auto cameraLocalMatrixXLen = glm::length(glm::vec3(viewMatrix[0]));
+		auto cameraLocalMatrixYLen = glm::length(glm::vec3(viewMatrix[1]));
+		auto cameraLocalMatrixZLen = glm::length(glm::vec3(viewMatrix[2]));
 
-			viewProjMatrix = projMatrix * viewMatrix;
+		auto viewMatrixBase = glm::inverse(modelMatrixMap.at(cameraNode));
+
+		auto cameraPrarentMatrix = glm::transpose(std::_Bit_cast<RTLib::Core::Matrix4x4>(cameraNode->mTransformation));
+
+		auto cameraParentMatrixXLen = glm::length(glm::vec3(cameraPrarentMatrix[0]));
+		auto cameraParentMatrixYLen = glm::length(glm::vec3(cameraPrarentMatrix[1]));
+		auto cameraParentMatrixZLen = glm::length(glm::vec3(cameraPrarentMatrix[2]));
+
+		viewMatrix = viewMatrix * viewMatrixBase;
+
+		auto cameraMatrixXLen = glm::length(glm::vec3(viewMatrix[0]));
+		auto cameraMatrixYLen = glm::length(glm::vec3(viewMatrix[1]));
+		auto cameraMatrixZLen = glm::length(glm::vec3(viewMatrix[2]));
+
+		// OPENGLが想定しているFOVはY軸まわり, 一方でASSIMPが想定しているFOVはX軸まわり
+		// なので結果を変える必要有
+		// X = ASPECT * TAN_Y
+		// Y = TAN_Y
+		// 
+		// X = TAN_X
+		// Y = TAN_X / ASPECT
+		// 注意: cameraのmClipPlaneNear, mClipPlaneFarは正規化された空間におけるFrustrum(0~1)ではなく
+		// ビュー空間における値
+		// そのため, View行列の深度方向のスケールをかける必要あり
+
+		float fovy = std::atan(std::tan(camera->mHorizontalFOV / 2.0f) / camera->mAspect) * 2.0f;
+		auto projMatrix = glm::perspective(fovy, camera->mAspect, camera->mClipPlaneNear, camera->mClipPlaneFar);
+
+		auto viewProjMatrix = projMatrix * viewMatrix;
+		{
 			std::cout << glm::to_string(viewProjMatrix) << std::endl;
 		}
 
 		//{
-		//	for (RTLib::UInt64 i = 0; i < scene->mNumMeshes; ++i) {
-		//		auto mesh = scene->mMeshes[i];
-		//		for (RTLib::UInt64 j = 0; j < mesh->mNumVertices; ++j) {
-		//			auto vertex = RTLib::Vector3();
-		//			vertex.x = mesh->mVertices[j][0] ;
-		//			vertex.y = mesh->mVertices[j][1] ;
-		//			vertex.z = mesh->mVertices[j][2] ;
-		//			auto viewVertex = viewMatrix * RTLib::Vector4(vertex, 1.0f);
-		//			std::cout << "VV: " << viewVertex.x << "," << viewVertex.y << "," << viewVertex.z << std::endl;
 
+		//	for (auto& [node, modelMatrix] : modelMatrixMap)
+		//	{
+
+		//		for (RTLib::UInt64 i = 0; i < node->mNumMeshes; ++i) {
+		//			auto mesh = scene->mMeshes[node->mMeshes[i]];
+		//			for (RTLib::UInt64 j = 0; j < mesh->mNumVertices; ++j) {
+		//				auto vertex = RTLib::Vector3();
+		//				vertex.x = mesh->mVertices[j][0];
+		//				vertex.y = mesh->mVertices[j][1];
+		//				vertex.z = mesh->mVertices[j][2];
+		//				auto worldVertex = modelMatrix * RTLib::Vector4(vertex, 1.0f);
+		//				std::cout << "VW: " << worldVertex.x << "," << worldVertex.y << "," << worldVertex.z << std::endl;
+
+		//			}
 		//		}
 		//	}
-		//	for (RTLib::UInt64 i = 0; i < scene->mNumMeshes; ++i) {
-		//		auto mesh = scene->mMeshes[i];
-		//		for (RTLib::UInt64 j = 0; j < mesh->mNumVertices; ++j) {
-		//			auto vertex = RTLib::Vector3();
-		//			vertex.x = mesh->mVertices[j][0];
-		//			vertex.y = mesh->mVertices[j][1];
-		//			vertex.z = mesh->mVertices[j][2];
-		//			auto viewVertex = viewMatrix * RTLib::Vector4(vertex, 1.0f);
-		//			auto projVertex = projMatrix * viewVertex;
-		//			projVertex /= projVertex.w;
-		//			std::cout << "VP: " << projVertex.x << "," << projVertex.y << "," << projVertex.z << std::endl;
+		//	for (auto& [node, modelMatrix] : modelMatrixMap)
+		//	{
 
+		//		for (RTLib::UInt64 i = 0; i < node->mNumMeshes; ++i) {
+		//			auto mesh = scene->mMeshes[node->mMeshes[i]];
+		//			for (RTLib::UInt64 j = 0; j < mesh->mNumVertices; ++j) {
+		//				auto vertex = RTLib::Vector3();
+		//				vertex.x = mesh->mVertices[j][0];
+		//				vertex.y = mesh->mVertices[j][1];
+		//				vertex.z = mesh->mVertices[j][2];
+		//				auto viewVertex = viewMatrix * modelMatrix * RTLib::Vector4(vertex, 1.0f);
+		//				std::cout << "VV: " << viewVertex.x << "," << viewVertex.y << "," << viewVertex.z << std::endl;
+
+		//			}
+		//		}
+		//	}
+		//	for (auto& [node, modelMatrix] : modelMatrixMap)
+		//	{
+
+		//		for (RTLib::UInt64 i = 0; i < node->mNumMeshes; ++i) {
+		//			auto mesh = scene->mMeshes[node->mMeshes[i]];
+		//			for (RTLib::UInt64 j = 0; j < mesh->mNumVertices; ++j) {
+		//				auto vertex = RTLib::Vector3();
+		//				vertex.x = mesh->mVertices[j][0];
+		//				vertex.y = mesh->mVertices[j][1];
+		//				vertex.z = mesh->mVertices[j][2];
+		//				auto viewVertex = viewMatrix * modelMatrix * RTLib::Vector4(vertex, 1.0f);
+		//				auto projVertex = projMatrix * viewVertex;
+		//				projVertex /= projVertex.w;
+		//				std::cout << "VP: " << projVertex.x << "," << projVertex.y << "," << projVertex.z << std::endl;
+
+		//			}
 		//		}
 		//	}
 		//}
 		gl->Enable(GL_DEPTH_TEST);
 		gl->DepthFunc(GL_LESS);
-		gl->DepthRangef(-1.0f, 1.0f);
-		gl->DepthMask(GL_FALSE);
-		gl->Enable(GL_CULL_FACE_MODE);
+
 		while (!glfwWindowShouldClose(window))
 		{
 			gl->Clear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			gl->ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+			gl->ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 			gl->ClearDepth(1.0f);
-			gl->Viewport(0,0,width,height);
+			gl->Viewport(0, 0, width, height);
 			gl->UseProgram(prg);
 			gl->UniformMatrix4fv(viewProjPos, 1, GL_FALSE, &viewProjMatrix[0][0]);
 			std::unordered_set<RTLib::UInt32> alreadyDrawSet = {};
-			for (auto& [node,model]:modelMatrixMap)
+			for (auto& [node, model] : modelMatrixMap)
 			{
-				gl->UniformMatrix4fv(modelPos, 1, GL_TRUE, &model[0][0]);
+				gl->UniformMatrix4fv(modelPos, 1, GL_FALSE, &model[0][0]);
 				for (auto i = 0; i < node->mNumMeshes; ++i) {
-					
+
 					RTLib::UInt32 meshIdx = node->mMeshes[i];
 					if (alreadyDrawSet.count(meshIdx) > 0) { continue; }
 					std::mt19937 mt1(meshIdx);
 					std::mt19937 mt(mt1);
-					std::uniform_real_distribution<float> uni(0.0f,1.0f);
+					std::uniform_real_distribution<float> uni(0.0f, 1.0f);
 					RTLib::Vector4 vec(uni(mt), uni(mt), uni(mt), 1.0f);
 					GLuint vao = meshVaos.at(meshIdx);
 					GLuint cnt = meshIbos.at(meshIdx).second;
 					gl->Uniform4fv(colorPos, 1, &vec[0]);
 					gl->BindVertexArray(vao);
-					gl->DrawElements(GL_TRIANGLES, scene->mMeshes[meshIdx]->mNumFaces*3, GL_UNSIGNED_INT, 0);
+					gl->DrawElements(GL_TRIANGLES, scene->mMeshes[meshIdx]->mNumFaces * 3, GL_UNSIGNED_INT, 0);
 					alreadyDrawSet.insert(meshIdx);
 				}
 			}
@@ -343,7 +401,7 @@ int main(int argc, const char** argv)
 					auto rightLen = std::sqrt(right.Length());
 					right.Normalize();
 					right *= rightLen;
-					camera->mPosition -= cameraVelocity *right;
+					camera->mPosition -= cameraVelocity * right;
 
 				}
 				if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
@@ -368,26 +426,23 @@ int main(int argc, const char** argv)
 					camera->mPosition -= cameraVelocity * camera->mUp;
 				}
 				{
-					viewMatrix = RTLib::Matrix4x4();
-					auto tmp = aiMatrix4x4();
-					camera->GetCameraMatrix(tmp);
-					auto tmp2 = modelMatrixMap[cameraNode];
-					std::memcpy(&viewMatrix, &tmp2, sizeof(tmp2));
-
-					viewMatrix = glm::inverse(glm::transpose(viewMatrix));
 					viewMatrix = glm::lookAt(
 						glm::vec3(camera->mPosition[0], camera->mPosition[1], camera->mPosition[2]),
-						glm::normalize(glm::vec3(camera->mLookAt[0], camera->mLookAt[1], camera->mLookAt[2])),
-						glm::normalize(glm::vec3(camera->mUp[0], camera->mUp[1], camera->mUp[2]))
-					);
+						glm::vec3(camera->mPosition[0], camera->mPosition[1], camera->mPosition[2]) +
+						glm::vec3(camera->mLookAt[0]  , camera->mLookAt[1]  , camera->mLookAt[2]),
+						glm::vec3(camera->mUp[0], camera->mUp[1], camera->mUp[2])
+					) * viewMatrixBase;
+					// GLMが想定しているFOVはY軸まわり, 一方でASSIMPが想定しているFOVはX軸まわり
+					// なので結果を変える必要有
+					// X = ASPECT * TAN_Y
+					// Y = TAN_Y
+					// 
+					// X = TAN_X
+					// Y = TAN_X / ASPECT
+					fovy = std::atan(std::tan(camera->mHorizontalFOV / 2.0f) / camera->mAspect) * 2.0f;
+					projMatrix = glm::perspective(fovy, camera->mAspect, camera->mClipPlaneNear, camera->mClipPlaneFar);
 
-					projMatrix = glm::perspective(
-						camera->mHorizontalFOV, camera->mAspect, camera->mClipPlaneNear, camera->mClipPlaneFar
-					);
-
-					viewProjMatrix = RTLib::Core::Matrix4x4();
 					viewProjMatrix = projMatrix * viewMatrix;
-					std::cout << glm::to_string(viewProjMatrix) << std::endl;
 				}
 			}
 		}
@@ -395,7 +450,7 @@ int main(int argc, const char** argv)
 			gl->DeleteVertexArrays(meshVaos.size(), meshVaos.data());
 			gl->DeleteBuffers(meshVbos.size(), meshVbos.data());
 			for (auto& [ibo, cnt] : meshIbos) {
-				gl->DeleteBuffers(1,&ibo);
+				gl->DeleteBuffers(1, &ibo);
 			}
 		}
 		gl->DeleteProgram(prg);
